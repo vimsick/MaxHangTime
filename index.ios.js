774255be2d0fc82b8ad9074e63e4d26a09835da7
Karin Kubischta <karin.kubischta@gmail.com>
2017-01-04 16:25:10 -0800

    homescreen, with buttons, onPress callbacks working

--- index.ios.js
+++ index.ios.js
@@ -9,23 +9,45 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  TouchableHighlight,
 } from 'react-native';
 
 export default class MaxHangTime extends Component {
+  onStartPressed() {
+    console.log('>>> Start Button Pressed!');
+  }
+
+  onEditPressed() {
+    console.log('>>> Edit Button Pressed!');
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          Welcome!
         </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        <View style={styles.flowRight}>
+          <TouchableHighlight
+            style={styles.button}
+            underlayColor='#99d9f4'
+            onPress={this.onStartPressed.bind(this)}
+          >
+            <Text style={styles.buttonText}>
+              Start Routine
+            </Text>
+          </TouchableHighlight>
+          <TouchableHighlight
+            style={[styles.button, styles.buttonEdit]}
+            underlayColor='#99d9f4'
+            onPress={this.onEditPressed.bind(this)}
+          >
+            <Text style={styles.buttonText}>
+              Edit Routine
+            </Text>
+          </TouchableHighlight>
+        </View>
       </View>
     );
   }
@@ -36,18 +58,46 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: 'black',
   },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
     margin: 10,
+    color: 'gray'
   },
   instructions: {
     textAlign: 'center',
     color: '#333333',
     marginBottom: 5,
   },
+  flowRight: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    alignSelf: 'stretch'
+  },
+  buttonText: {
+    fontSize: 18,
+    color: 'white',
+    alignSelf: 'center'
+  },
+  button: {
+    height: 36,
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: '#48BBEC',
+    borderColor: '#48BBEC',
+    borderWidth: 1,
+    borderRadius: 8,
+    // marginBottom: 10,
+    margin: 10,
+    alignSelf: 'auto',
+    justifyContent: 'center'
+  },
+  buttonEdit: {
+    backgroundColor: 'green',
+    borderColor: 'green',
+  }
 });
 
 AppRegistry.registerComponent('MaxHangTime', () => MaxHangTime);
